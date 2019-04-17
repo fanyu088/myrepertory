0c910a91795b6a6323740c8219c1cde907761e0e
fanyu088 <fanyu088@outlook.com>
2019-04-18 00:22:22 +0800

    dai

--- src/main/java/com/example/demo12Stream/Random_test.java
+++ src/main/java/com/example/demo12Stream/Random_test.java
@@ -3,10 +3,12 @@ package com.example.demo12Stream;
 public class Random_test {
     public static void main(String[] args) {
 
-        int i;
+        int i = 1;
         for (int j = 0; j < 20; j++) {
             System.out.println( (int) (Math.random() * 10));
         }
+
+        System.out.println(i);
 
 
     }
